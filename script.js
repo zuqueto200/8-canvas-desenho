e4a49547c715d6b415f35d54adcec4fea3f112ae
zuqueto200 <ricardozuqueto@gmail.com>
2022-02-04 22:29:22 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,59 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const pincel = {
+        ativo: false,
+        movendo: false,
+        pos: { x: 0, y: 0 },
+        posAnterior: null
+    }
+    const tela = document.querySelector('.tela');
+    const contexto = tela.getContext('2d');
+
+    tela.width = 500;
+    tela.height = 300;
+    contexto.lineWidth = 3;
+    contexto.strokeStyle = "Blue"
+
+    const desenharLinha = (linha) => {
+
+        contexto.beginPath();
+        contexto.moveTo(linha.posAnterior.x, linha.posAnterior.y);
+        contexto.lineTo(linha.pos.x, linha.pos.y);
+        contexto.stroke();
+    }
+
+    tela.onmousedown = (evento) => { pincel.ativo = true }
+    tela.onmouseup = (evento) => { pincel.ativo = false }
+    tela.onmousemove = (evento) => {
+        pincel.pos.x = evento.clientX
+        pincel.pos.y = evento.clientY
+        pincel.movendo = true;
+
+    }
+    const ciclo = () => {
+        if (pincel.ativo && pincel.movendo && pincel.posAnterior) {
+            desenharLinha({ pos: pincel.pos, posAnterior: pincel.posAnterior })
+            pincel.movendo = false;
+
+        }
+        pincel.posAnterior = { x: pincel.pos.x, y: pincel.pos.y }
+        setTimeout(ciclo, 3);
+    }
+
+    ciclo()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
